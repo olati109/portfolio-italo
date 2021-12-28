6cca311b2e30741326f57504ee9e21f9900e8539
italo <italocoelho109@gmail.com>
2021-12-28 14:22:19 -0300

    terminado o portfolio

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //efeito do parallax
-
 window.addEventListener('scroll', function () {
   var header = this.document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
@@ -17,7 +16,6 @@ function parallax(e) {
 }
 
 //Scroll Suave para Link Interno
-
 const menuItems = document.querySelectorAll('.nav-list a[href^="#"]');
 
 menuItems.forEach((item) => {
@@ -31,7 +29,48 @@ function scrollToIdOnClick(event) {
   const to = document.querySelector(id).offsetTop;
 
   window.scroll({
-    top: to,
+    top: to - 80,
     behavior: 'smooth',
   });
 }
+
+// anima ao scroll
+// const debounce = function (func, wait, immediate) {
+//   let timeout;
+//   return function (...args) {
+//     const context = this;
+//     const later = function () {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     const callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// };
+
+// const target = document.querySelectorAll('[data-anime]');
+// const animationClass = 'animate';
+
+// function animeScroll() {
+//   const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+//   target.forEach(function (element) {
+//     if (windowTop > element.offsetTop) {
+//       element.classList.add(animationClass);
+//     } else {
+//       element.classList.remove(animationClass);
+//     }
+//   });
+// }
+
+// animeScroll();
+
+// if (target.length) {
+//   window.addEventListener(
+//     'scroll',
+//     debounce(function () {
+//       animeScroll();
+//     }, 200),
+//   );
+// }
